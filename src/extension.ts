9aa84e55113a9e91a8f0c3cbd0f8113dcabadd71
nagata-yoshiteru <nagata.yoshiteru@g.mbox.nagoya-u.ac.jp>
2021-04-24 22:53:28 +0900

    behavior change when opening unsupported file, Image information display, refactor

--- src/extension.ts
+++ src/extension.ts
@@ -8,51 +8,52 @@ export function activate(context: vscode.ExtensionContext) {
         (document: vscode.TextDocument) => {
 			if (document.fileName.endsWith(".ppm") || document.fileName.endsWith(".pgm")) {
 				console.log(`Opened ${document.fileName}`);
-				
-				vscode.commands.executeCommand('workbench.action.closeActiveEditor').then(() => {
-					const spPath = document.uri.path.split("/");
-					if (document.uri.fsPath) {
-						const byteData = fs.readFileSync(document.uri.fsPath);
-		
-						let i = 0, j = 0, imgType = "P5", width = 1, height = 1, mc = 1, k = 0;
-						while (i < byteData.byteLength && k < 3) {
-							while (i < byteData.byteLength && byteData[i++] !== 10);
-							if (i - 1 - j === 2) {
-								imgType = byteData.toString('utf-8', j, i - 1);
-								console.log(`Type: ${imgType}`);
-								k++;
-							} else {
-								const texts = byteData.toString('utf-8', j, i - 1).split(" ");
-								if (texts.length === 1) {
-									mc = Number(texts[0]);
-									console.log(`Color: ${mc}`);
-									k++;
-								} else if (texts.length === 2) {
-									width = Number(texts[0]);
-									height = Number(texts[1]);
-									console.log(`Size: ${width}x${height}`);
-									k++;
-								}
-							}
-							j = i;
-						}
-		
-						let colorData: { red: number, green: number, blue: number }[] = [];
-						switch (imgType) {
-							case "P5":
-								for (let index = i; index < byteData.byteLength; index++) {
-									colorData.push({ red: byteData[index], green: byteData[index], blue: byteData[index] });
-								}
-								break;
-							case "P6":
-								for (let index = i; index < byteData.byteLength; index = index + 3) {
-									colorData.push({ red: byteData[index], green: byteData[index + 1], blue: byteData[index + 2] });
-								}
-								break;
-						}
-		
-						const data = { width, height, colorData };
+				const spPath = document.uri.path.split("/");
 
+				if (document.uri.fsPath) {
+					const byteData = fs.readFileSync(document.uri.fsPath);
+	
+					let i = 0, j = 0, imgType = "P0", width = 1, height = 1, mc = 1, k = 0;
+					while (i < byteData.byteLength && k < 3) {
+						while (i < byteData.byteLength && byteData[i++] !== 10);
+						if (i - 1 - j === 2) {
+							imgType = byteData.toString('utf-8', j, i - 1);
+							console.log(`Type: ${imgType}`);
+							k++;
+						} else {
+							const texts = byteData.toString('utf-8', j, i - 1).split(" ");
+							if (texts.length === 1) {
+								mc = Number(texts[0]);
+								console.log(`Color: ${mc}`);
+								k++;
+							} else if (texts.length === 2) {
+								width = Number(texts[0]);
+								height = Number(texts[1]);
+								console.log(`Size: ${width}x${height}`);
+								k++;
+							}
+						}
+						j = i;
+					}
+	
+					let colorData: { r: number, g: number, b: number }[] = [];
+					switch (imgType) {
+						case "P5":
+							for (let index = i; index < byteData.byteLength; index++) {
+								colorData.push({ r: byteData[index], g: byteData[index], b: byteData[index] });
+							}
+							break;
+						case "P6":
+							for (let index = i; index < byteData.byteLength; index = index + 3) {
+								colorData.push({ r: byteData[index], g: byteData[index + 1], b: byteData[index + 2] });
+							}
+							break;
+						default:
+							return;
+					}
+
+					vscode.commands.executeCommand('workbench.action.closeActiveEditor').then(() => {
+						const data = { width, height, colorData, imgType };
 						const panel = vscode.window.createWebviewPanel(
 							'.ppm preview',
 							spPath[spPath.length - 1],
@@ -61,67 +62,41 @@ export function activate(context: vscode.ExtensionContext) {
 								enableScripts: true,
 							}
 						);
-						panel.webview.html = generateHTMLCanvas(JSON.stringify(data));
-					}
-				});
+						panel.webview.html = generateHTMLCanvas(JSON.stringify(data), width, height, imgType);
+					});
+				}
 			}
         });
 	
     context.subscriptions.push(openedEvent);
 }
 
-/**
- * recursively searches a directory structure for all files.
- * @param dir 
- */
-var walk = function (dir: string): string[] {
-	var results: string[] = [];
-	var list = fs.readdirSync(dir);
-	list.forEach(function (file) {
-		file = dir + '/' + file;
-		var stat = fs.statSync(file);
-		if (stat && stat.isDirectory()) {
-			/* Recurse into a subdirectory */
-			results = results.concat(walk(file));
-		} else {
-			/* Is a file */
-			results.push(file);
-		}
-	});
-	return results;
-};
-
-function generateHTMLCanvas(data: string): string {
+function generateHTMLCanvas(data: string, width: number, height: number, imgType: string): string {
 	return `<!DOCTYPE html>
 	<html lang="en">
 	<head>
 		<meta charset="UTF-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	</head>
-	<body>	
+	<body>
 		<script>
 			var jsonStr = '${data}';
 			var canvas = document.createElement('canvas');
 			document.body.appendChild(canvas);
 			var ctx = canvas.getContext('2d');
 			const message = JSON.parse(jsonStr);
-			var width = message.width;
-			var height = message.height;
 			var colorData = message.colorData;
-			canvas.width = width;
-			canvas.height = height;
-			for (var x = 0; x < width; x++){
-				for (var y = 0; y < height; y++){
-					//depending on max value
-					var color = colorData[(y * width) + x];
-					var r = color.red;
-					var g = color.green;
-					var b = color.blue;
-					ctx.fillStyle = "rgba(" + r + "," + g + "," + b + "," + 1.0 + ")";
+			canvas.width = ${width};
+			canvas.height = ${height};
+			for (var x = 0; x < ${width}; x++){
+				for (var y = 0; y < ${height}; y++){
+					var color = colorData[(y * ${width}) + x];
+					ctx.fillStyle = "rgba(" + color.r + "," + color.g + "," + color.b + "," + 1.0 + ")";
 					ctx.fillRect(x, y, 1, 1);
 				}
 			}
 		</script>
+		<p>Width: ${width}px, Height: ${height}px, Type: ${imgType}</p>
 	</body>
 	</html>`;
 }
